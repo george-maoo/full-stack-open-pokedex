3f467a0c36d64a2c0927ddd8c3fe74ba4f755713
dotaplayer1 <georgemao72@gmail.com>
2023-08-04 14:56:33 -0700

    Remove error in health check

--- app.js
+++ app.js
@@ -7,12 +7,10 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/version', (req, res) => {
-  res.send('1') // change this string to ensure a new version deployed
+  res.send('2') // change this string to ensure a new version deployed
 })
 
 app.get('/health', (req, res) => {
-  throw new Error()
-  // eslint-disable-next-line
   res.send('ok')
 })
 
