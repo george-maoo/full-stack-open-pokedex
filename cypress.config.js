a60dc3a66c96242a178062b7f463734a3d6034b6
dotaplayer1 <georgemao72@gmail.com>
2023-08-04 12:51:45 -0700

    Add another e2e test

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
+    video: false,
     // eslint-disable-next-line
     setupNodeEvents(on, config) {
       // implement node event listeners here
